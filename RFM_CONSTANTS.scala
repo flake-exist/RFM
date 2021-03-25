58f054617127fd2cc097f2a759dac19f1539aed3
flake-exist <65955746+flake-exist@users.noreply.github.com>
2021-03-25 18:14:04 +0300

    Add files via upload

--- RFM_CONSTANTS.scala
+++ RFM_CONSTANTS.scala
@@ -1,17 +1,36 @@
 object RFM_CONSTANTS {
-  val reformChain = (chain:String,ch_sep:String,utm_sep:String,sp_symbol:String) => {
-    val delimiter = sp_symbol + utm_sep
-    val ch_list   = chain.split(ch_sep)
-    val arr       = ch_list.foldLeft(List.empty[String]) {
-      case (acc,i) if acc.isEmpty                                               => List(i)
-      case (acc,i) if acc.last.startsWith(delimiter) & !i.startsWith(delimiter) => acc
-      case (acc,i)                                                              => acc :+ i
+
+  val reformChain = (ch_hts:Seq[Seq[String]],
+                     utm_sep:String,
+                     alert_symbol:String,
+                     session_symbol:String) => {
+
+    val alert_seq = alert_symbol + utm_sep
+    val arr = ch_hts.foldLeft(List.empty[Map[String,String]]) {
+      case (acc,i) if acc.isEmpty                                                               => List(Map(i(0) -> i(1)))
+      case (acc,i) if acc.last.keys.head.startsWith("click_>>_") & i(0).startsWith("session")   => acc
+      case (acc,i) if acc.last.keys.head.startsWith("click_>>_") & i(0).startsWith("click_>>_") => acc
+      case (acc,i)                                                                              => acc :+ Map(i(0) -> i(1))
     }
-    val reform_chain:String = arr.mkString(ch_sep)
-    reform_chain
+//    val reform_chain:String = arr.mkString(ch_sep)
+    arr
   }
+
+  val getChannelR = (ch_hts:Seq[Map[String,String]]) => {
+    val channel_seq:Seq[String] = ch_hts.map(_.keys.head)
+    channel_seq
+
+  }
+
+  val getTimelineR = (ch_hts:Seq[Map[String,String]]) => {
+    val timeline:Seq[String]     = ch_hts.map(_.values.head)
+    timeline
+  }
+
+
 
   val CLICK:String     = "click"
   val VIEW:String      = "view"
+  val SESSION:String   = "session"
   val USER_PATH:String = "user_path"
 }
--- RFM_chain.scala
+++ RFM_chain.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.concat_ws
 
 //args[0] - input filepath
 //args[1] - output folder
@@ -14,34 +15,58 @@ object RFM_chain {
     val spark = SparkSession.builder.appName("Reform Chain").getOrCreate()
     import spark.implicits._
 
-        val reformChain_udf = spark.udf.register("reformChain_udf",reformChain)
-        val type_report:String = args(2) match {
-          case a@"detail" => a
-          case b@"agg"    => b
-          case e@_        => throw new Exception(s"$e mode does not exist")
-        }
+    val reformChain_udf      = spark.udf.register("reformChain_udf",reformChain)
+    val getChannelR_udf      = spark.udf.register("getChannelR_udf",getChannelR)
+    val getTimelineR_udf     = spark.udf.register("getTimelineR_udf",getTimelineR)
 
-        val data = spark.read.
-          format("csv").
-          option("inferSchema","false").
-          option("header","true").
-          option("mergeSchema","true").
-          load(args(0))
+    val type_report:String = args(2) match {
+      case a@"detail" => a
+      case b@"agg"    => b
+      case e@_        => throw new Exception(s"$e mode does not exist")
+    }
 
-        data.show(20)
+    val data = spark.read.
+      format("csv").
+      option("inferSchema","false").
+      option("header","true").
+      option("mergeSchema","true").
+      load(args(0))
 
-        val data_0 = data.
-          withColumn("user_pathR", reformChain_udf(col(USER_PATH), lit(TRANSIT), lit(CONCAT_SYMBOL), lit(CLICK)))
+    data.show(20)
 
-        type_report match {
-          case "detail" => {
-            data_0.write.format("csv").option("header", "true").mode("overwrite").save(args(1))
-          }
-          case "agg"   => {
-            data_0.coalesce(1).write.format("csv").option("header", "true").mode("overwrite").save(args(1))
-          }
-          case e@_     => throw new Exception(s"$e report type  does not exist")
-        }
+    val data_seq = data.
+      withColumn("channels",split($"user_path",TRANSIT)).
+      withColumn("date_touch",split($"timeline",TRANSIT)).
+      withColumn("touch_data",arrays_zip($"channels",$"date_touch"))
+
+    val data_reform = data_seq.
+      withColumn("touch_dataR", reformChain_udf($"user_path",lit(CONCAT_SYMBOL),lit(CLICK), lit(SESSION)))
+
+
+
+    val data_seqR = data_reform.
+      withColumn("user_pathR_seq",getChannelR_udf($"touch_dataR")).
+      withColumn("timelineR_seq",getTimelineR_udf($"touch_dataR"))
+
+    val data_result = data_seqR.
+      withColumn("user_pathR",concat_ws(TRANSIT,$"user_pathR_seq")).
+      withColumn("timelineR",concat_ws(TRANSIT,$"timelineR_seq")).
+      select(
+        $"ClientID",
+        $"user_pathR",
+        $"timelineR"
+      )
+
+    type_report match {
+      case "detail" => {
+        data_result.write.format("csv").option("header", "true").mode("overwrite").save(args(1))
+      }
+      case "agg"   => {
+        data_result.coalesce(1).write.format("csv").option("header", "true").mode("overwrite").save(args(1))
+      }
+      case e@_     => throw new Exception(s"$e report type  does not exist")
+    }
 
   }
 }
+
